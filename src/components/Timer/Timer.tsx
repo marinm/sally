3fedfa585d12a4b49e593deba599ff3b1c108fed
Marin Mema <marinmema@live.ca>
2025-03-16 13:09:42 -0400

    show progress as line

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -9,7 +9,7 @@ import Stop from '../icons/Stop';
 function dotClass(dot: Dot, iCurrent: number = -1) {
     return classes({
         dot: true,
-        'dot-is-hold': ['H', 'P', '★'].includes(dot.display),
+        'dot-is-hold': dot.isHold,
         'dot-in-progress': iCurrent == dot.i,
         'dot-is-done': iCurrent > dot.i,
     });
@@ -44,14 +44,14 @@ export default function Timer() {
                 onTimeUpdate={() => setElapsed(audio.current?.currentTime ?? 0)}
                 ref={audio}
             ></audio>
-            <div>{timeFormat(elapsed)}</div>
+            <div>{DOTS[iCurrent]?.display ?? 'Bring Sally Up'}</div>
             <div className={classes({ dots: true })}>
                 {DOTS.map((dot) => (
-                    <div className={dotClass(dot, iCurrent)} key={dot.i}>
-                        {dot.display}
-                    </div>
+                    <div className={dotClass(dot, iCurrent)} key={dot.i}></div>
                 ))}
             </div>
+            <br />
+            <div>{timeFormat(elapsed)}</div>
             <div className="btn-group">
                 <button
                     onClick={() => audio.current?.play()}
